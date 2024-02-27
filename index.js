686236714de85c453cc8488367cae4942acec4eb
Nwankwo Chidera <nwankwochidera.david@gmail.com>
2024-02-27 01:29:12 +0100

    Test on the server

--- index.js
+++ index.js
@@ -36,12 +36,13 @@ const makeRequest = async () => {
   });
 
 
-  const data = response.data;
+  const data = response.data.events;
+  console.log(data);
   for (let i = 0; i < data.length; i++) {
     const match = data[i];
     const matchLink = `https://www.sofascore.com/${match.slug}/${match.customId}#id:${match
       .tournament.id}`;
-    if (!matchesWithRedCard.includes(teams)) {
+    if (!matchesWithRedCard.includes(matchLink)) {
       if (data[i].homeRedCards > 0 || data[i].awayRedCards > 0) {
         const homeTeam = match.homeTeam.name;
         const awayTeam = match.awayTeam.name;
@@ -63,9 +64,9 @@ const makeRequest = async () => {
   }
 };
 
-setInterval(() => {
-  makeRequest();
-}, 10000);
+// setInterval(() => {
+//   makeRequest();
+// }, 10000);
 
 app.listen(PORT, () => {
   console.log("Server Listening at PORT: ", PORT);
